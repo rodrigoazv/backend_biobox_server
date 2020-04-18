267e48a2cd890676f9e1f604296ef8363f7b5d44
Rodrigo Azevedo <rodrigozcma@gmail.com>
2020-04-17 21:16:18 -0300

    Adicionando relaçoes de adress controller

--- src/controllers/adressController.ts
+++ src/controllers/adressController.ts
@@ -1,1 +1,46 @@
 
+import { Request, Response, Router } from 'express';
+import { getManager } from 'typeorm';
+//import service
+import { AdressService } from '../service/adressService';
+//import user entity
+
+import { Adress } from '../entity/adressEntity';
+
+
+const adressRoutes: Router = Router();
+/*
+ * Checks whether the login already exists
+ *
+ * @Method GET
+ * @URL /api/users/
+ *
+*/
+adressRoutes
+    .route('/adress')
+    .post(
+    async (req: Request, res:Response) =>{
+        try{
+            let adressNew = new Adress();
+            adressNew.zipcode = req.body.zipcode;
+            adressNew.street = req.body.street;
+            adressNew.number = req.body.number;
+            adressNew.neighborhood = req.body.neighborhood;
+            adressNew.complement = req.body.complement;
+
+            const adressService = new AdressService();
+            const adressRepository = getManager().getRepository(Adress);
+            adressNew = adressRepository.create(adressNew);
+            adressNew = await adressService.insertOne(adressNew);
+            res.json({
+                message:"Endereço cadastrado",
+            })
+        }catch{
+                res.json({
+                    message:"erro",
+                })
+        }
+    }
+)
+
+export default adressRoutes;
--- src/routes.ts
+++ src/routes.ts
@@ -6,11 +6,13 @@ import {Router} from 'express';
 import userRoutes from './controllers/userController';
 import registerRoutes from './controllers/registerController';
 import productRoutes from './controllers/productController';
+import adressRoutes from './controllers/adressController';
 
 const routes: Router = Router();
 
 routes.use('/', userRoutes);
 routes.use('/user', registerRoutes);
 routes.use('/product', productRoutes);
+routes.use('/cad', adressRoutes);
 
 module.exports = routes;
